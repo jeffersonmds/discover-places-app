14630d19af39ecb2a08d0ec66bccc96fb33515c8
Jefferson Morais <jeffmorais96@gmail.com>
2019-07-22 22:45:45 -0300

    getting local position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
-import { SourceRasterComponent } from 'ngx-openlayers';
-import { GeometryPointComponent } from 'ngx-openlayers';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MapComponent, FeatureComponent } from 'ngx-openlayers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{  
+  @ViewChild(MapComponent, {static: false}) map : MapComponent;
+  public longitude : number = -45.89279472860653;
+  public latitude : number = -23.196065627947135;
+  
+  ngOnInit(): void {
+    
+  }
+   
+
+  onMapClick(event: ol.MapBrowserEvent){
+    // var feat : FeatureComponent;
+    // feat.instance = event.map;
+    // console.log(event);
+    // const map = event.map;
+  }
+
+  onGetLocalizationClick(){
+    if (window.navigator && window.navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        location => {
+          this.latitude = Number(location.coords.latitude.toPrecision());
+          this.longitude = Number(location.coords.longitude.toPrecision());
+        },
+        error => {
+          switch (error.code) {
+            case 1:
+              console.log('Permission Denied');
+              break;
+            case 2:
+              console.log('Position Unavailable');
+              break;
+            case 3:
+              console.log('Timeout');
+              break;
+          }
+        }
+      );
+    }
+    else {
+      console.log('Geolocation API not supported.');
+    }
+  }
 }
